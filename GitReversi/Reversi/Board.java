970e02441955c82816d3c70cae2fd965a262a1e2
GozenAlf-sf <Alftheta@gmail.com>
2016-04-30 16:26:30 +0900

    4/30 16:26

--- GitReversi/Reversi/Board.java
+++ GitReversi/Reversi/Board.java
@@ -1,4 +1,19 @@
 package Reversi;
+
+
 public class Board {
+	public static final int BOARD_SIZE = 8;
+
+	//Boardの上にMasuを作る
+
+	//引数なしだとij動かし64個Masuをnewする
+
+	//引数3つだとij・・・ちょい違う（あとで
+
+	//onStone石が置かれたときの挙動
+
+	//turn over check stone position ???
+
+
 
 }
