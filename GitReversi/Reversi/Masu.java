f462b16ab7075b029693a07b438ce57ec4670cfd
GozenAlf-sf <Alftheta@gmail.com>
2016-05-01 18:34:29 +0900

    5/2

---
+++ GitReversi/Reversi/Masu.java
@@ -0,0 +1,17 @@
+package Reversi;
+public class Masu {
+
+
+
+	//コンストラクタ
+
+	//setStone Masuのstoneに引数のstoneを
+
+	//getStone stone型のstoneを返す
+
+	//removeStone
+
+	//toString 空か文字を返す（いらない？
+
+	//draw(これはGUI側でやる？
+}
