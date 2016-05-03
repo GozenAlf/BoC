4ddd281dd6513156a8e363272fe2169f6e11052f
GozenAlf-sf <Alftheta@gmail.com>
2016-05-03 14:42:09 +0900

    5/3

--- GitReversi/Reversi/Reversi/Board.java
+++ GitReversi/Reversi/Reversi/Board.java
@@ -4,11 +4,50 @@ package Reversi;
 public class Board {
 	public static final int BOARD_SIZE = 8;
 
+	private int gWidth;
+	private int gHeight;
+
+	private Masu[][] masus = new Masu[BOARD_SIZE][BOARD_SIZE];
 	//Boardの上にMasuを作る
 
-	//引数なしだとij動かし64個Masuをnewする
+	public Board(int width, int height) { //参考コードは引数3つだけど２つでいいんじゃね？
+		setgWidth(width);	//パネルサイズの800が入る
+		setgHeight(height);
+		for (int i = 0; i < BOARD_SIZE; i++){
+			for (int j = 0; j < BOARD_SIZE; j++){
+				masus[i][j] = new Masu(getgWidth()/BOARD_SIZE,getgWidth()/BOARD_SIZE*i,getgHeight()/BOARD_SIZE*j);
+			}
+		}
 
-	//引数3つだとij・・・ちょい違う（あとで
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+	//以下getterとsetter
+	public int getgWidth() {
+		return gWidth;
+	}
+
+	public void setgWidth(int gWidth) {
+		this.gWidth = gWidth;
+	}
+
+	public int getgHeight() {
+		return gHeight;
+	}
+
+	public void setgHeight(int gHeight) {
+		this.gHeight = gHeight;
+	}
 
 	//onStone石が置かれたときの挙動
 
--- GitReversi/Reversi/Reversi/Masu.java
+++ GitReversi/Reversi/Reversi/Masu.java
@@ -1,5 +1,44 @@
 package Reversi;
 public class Masu {
+	
+	
+	private int gX;
+	private int gY;
+	private int gSize;
+
+	public Masu(int size, int x, int y) {
+		setgSize(size);
+		setgX(x);
+		setgY(y);
+		
+	}
+
+	
+	
+	
+	
+	public int getSize() {
+		return this.gSize;
+	}
+
+	public void setgSize(int size) {
+		this.gSize = size;
+	}
+	public int getgX() {
+		return this.gX;
+	}
+
+	public void setgX(int gX) {
+		this.gX = gX;
+	}
+
+	public int getgY() {
+		return this.gY;
+	}
+
+	public void setgY(int gY) {
+		this.gY = gY;
+	}
 
 
 
